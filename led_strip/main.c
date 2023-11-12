f42601254693c0231a21a83d687386326cf812bb
Mårten Kongstad <marten.kongstad@gmail.com>
2023-11-12 12:21:36 +0100

    led_strip: connect to WiFi
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- led_strip/main.c
+++ led_strip/main.c
@@ -11,10 +11,18 @@ int main() {
         fprintf(stderr, "cyw43_arch_init failed\n");
         return 1;
     }
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    cyw43_arch_enable_sta_mode();
+    printf("connecting...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD,
+                                           CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        fprintf(stderr, "failed to connect\n");
+        return -1;
+    } else {
+        printf("connected\n");
+    }
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
     while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(250);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(250);
+        tight_loop_contents();
     }
 }
