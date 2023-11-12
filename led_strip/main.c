7704afdc5aca37a0ec56355630a85b05353bdc4b
Mårten Kongstad <marten.kongstad@gmail.com>
2023-11-12 11:55:48 +0100

    Add support for Pico W hardware
    The led_strip project will use the WiFi chip of Pico W. Update the build
    files accordingly.
    
    To set up the build directory, run
    
      $ cmake -DPICO_BOARD=pico_w -DWIFI_SSID=... -DWIFI_PASSWORD=... <path-to-root>
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- led_strip/main.c
+++ led_strip/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+#include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 
 int main() {
     stdio_init_all();
-    printf("main.c: hello world\n");
+    printf("%s: WIFI_SSID='%s' WIFI_PASSWORD='%s'\n", __FILE__, WIFI_SSID,
+           WIFI_PASSWORD);
+    if (cyw43_arch_init()) {
+        fprintf(stderr, "cyw43_arch_init failed\n");
+        return 1;
+    }
     while (true) {
-        tight_loop_contents();
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
     }
 }
